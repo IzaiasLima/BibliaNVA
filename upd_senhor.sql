a27a20f645fce003d8a1eff9597d96ce9d0686f1
Izaias Moreira Lima <izaiasmlima@gmail.com>
2026-02-10 12:49:10 -0300

    Ajuste de formatacao

--- upd_senhor.sql
+++ upd_senhor.sql
@@ -1,15 +1,31 @@
--- Inicio da frase
+--
+-- Update de Yahweh para SENHOR em alguns textos
+--
+
+-- No inicio da frase
 --
 UPDATE bible
-SET text = REPLACE(text, 'Yahweh', 'O <mark>Senhor<mark>')
-WHERE text LIKE 'Yahweh%'
-    AND book = 19
-    AND chapter = 23;
+SET text = REPLACE(text, 'Yahweh ', 'O <mark>Senhor</mark> ')
+WHERE text LIKE 'Yahweh %';
 --
 -- No meio da frase (de para do)
 --
 UPDATE bible
-SET text = REPLACE(text, 'de Yahweh', 'do <mark>Senhor<mark>')
-WHERE text LIKE '%de Yahweh%'
-    AND book = 19
-    AND chapter = 23;
+SET text = REPLACE(text, 'de Yahweh', 'do <mark>Senhor</mark>')
+WHERE text LIKE '%de Yahweh%';
+--
+-- Para o Senhor dos Exércitos
+--
+UPDATE bible
+SET text = REPLACE(
+        text,
+        'diz Yahweh dos Exércitos',
+        'diz o <mark>Senhor</mark> dos Exércitos'
+    )
+WHERE text LIKE '%diz Yahweh dos Exércitos%';
+--
+-- Eliminar espacos duplos
+--
+UPDATE bible
+SET text = REPLACE(text, '  ', ' ')
+WHERE text LIKE '%  %';
