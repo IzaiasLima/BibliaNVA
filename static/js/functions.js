b1149769965d34308d6aac80fabd634aa7ba67b6
Izaias Moreira Lima <izaiasmlima@gmail.com>
2026-02-11 00:21:57 -0300

    Incluir a sinalizacao de capitulos lidos

--- static/js/functions.js
+++ static/js/functions.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
 }
 
 const input = document.getElementById("search");
-input.addEventListener('keyup', searchWords);
+if (input) input.addEventListener('keyup', searchWords);
 
 
 // Exibe o campo de pesquisa (ou realiza a pesquisa, se o campo já está preenchido)
@@ -33,17 +33,92 @@ function searchWords(evt) {
     }
 }
 
+// salva capitulos lidos
+function setReadChapters(book, chapter) {
+    const readChapters = JSON.parse(localStorage.getItem('readChapters')) || { books: [] };
+    const bookIndex = readChapters.books.findIndex(b => b.name === book);
+
+    if (bookIndex > -1) {
+        if (!readChapters.books[bookIndex].chapters.includes(chapter)) {
+            readChapters.books[bookIndex].chapters.push(chapter);
+        }
+    } else {
+        readChapters.books.push({ name: book, chapters: [chapter] });
+    }
+    localStorage.setItem('readChapters', JSON.stringify(readChapters));
+}
+
+// verifica se o capitulo foi lido
+function isReadChapters(book, chapter) {
+    const readChapters = JSON.parse(localStorage.getItem('readChapters')) || { books: [] };
+    const bookIndex = readChapters.books.findIndex(b => b.name === book);
+    return readChapters.books[bookIndex].chapters.includes(chapter);
+}
+
+// salva o altimo capitulo lido
+function setLastChapter(book, chapter) {
+    const lastState = { book: book, chapter: chapter };
+    localStorage.setItem('lastState', JSON.stringify(lastState));
+}
+
+// reexibe o ultiomo capitolo lido
+function getLastChapter() {
+    var lastState = JSON.parse(localStorage.getItem('lastState'))
+
+    if (!lastState) {
+        lastState = { book: 'SL', chapter: 23 };
+    }
+    chapterView(lastState.book, lastState.chapter);
+}
+
+function showBtnRead() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const btn = document.getElementById("btn-read");
+    const position = document.getElementById("btn-position");
+
+    if (btn && position) {
+        const book = btn.dataset.book;
+        const chapter = btn.dataset.chapter;
+
+        if (!isReadChapters(book, chapter)) {
+            if (scrollTop + clientHeight >= scrollHeight - 20) {
+                position.classList.add('show', 'animate__fadeInUp');
+            } else {
+                position.classList.remove('show', 'animate__fadeInUp');
+            }
+        }
+    }
+}
 
 document.addEventListener('htmx:responseError', evt => {
     error = JSON.parse(evt.detail.xhr.responseText);
     showToast(error.detail);
 });
 
-document.addEventListener('swiped-right', async function (evt) {
+
+document.addEventListener('swiped-up', async function () {
+    showBtnRead();
+    const btnChapter = document.getElementsByClassName("chapter");
+
+    Array.from(btnChapter).forEach(btn => {
+        const book = btn.dataset.book;
+        const chapter = btn.dataset.chapter;
+        if (isReadChapters(book, chapter)) {
+            btn.classList.add('bg-success');
+        }
+    });
+
+});
+
+
+document.addEventListener('swiped-right', async function () {
+    showSpinner();
     navigation(-1);
 });
 
-document.addEventListener('swiped-left', async function (evt) {
+
+document.addEventListener('swiped-left', async function () {
+    showSpinner();
     navigation(1);
 });
 
@@ -65,8 +140,10 @@ async function navigation(direction) {
         const thisBook = book.innerHTML.trim();
         const nextChapter = Number(chapter.innerHTML.trim()) + direction
 
-        if ((nextChapter >= 1) && (nextChapter <= total))
+        if ((nextChapter >= 1) && (nextChapter <= total)) {
             await chapterView(thisBook, nextChapter);
+            setLastChapter(thisBook, nextChapter);
+        }
     }
 };
 
@@ -128,7 +205,9 @@ async function chapterView(book, chapter) {
             if (!data.bookAbbr) return
 
             const template = document.getElementById('chapter-template').innerHTML;
-            const result = document.getElementById('chapter-render');
+            var result = document.getElementById('chapter-render')
+
+            result = (result) ? result : document.getElementById('data-render');
             result.innerHTML = Mustache.render(template, { data: data });
             htmx.process(result);
         }
